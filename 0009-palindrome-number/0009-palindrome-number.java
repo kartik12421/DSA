e42e87ada5447d29d7ef2c1bef0fe2b6350ab761
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-09-12 21:44:32 +0530

    Time: 5 ms (82.6%), Space: 46.3 MB (5.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // - wala
+        if (x < 0) {
+            return false;
+        };
+
+        int rev = 0;
+        int copy = x;
+
+        while (x != 0) {
+            rev = (rev * 10) + (x % 10);
+            x /= 10;
+        }
+
+        return rev == copy;
+    }
+}
